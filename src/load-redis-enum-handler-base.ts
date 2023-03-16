725f4f266c188934f383c8b3845a8657cdeac19e
linhongjue <linhongjue@fengling.cn>
2023-03-16 10:49:59 +0800

    feat:设置服务对应的枚举redis缓存

--- src/load-redis-enum-handler-base.ts
+++
@@ -1,5 +0,0 @@
-import { LoadEnumHandlerBase } from 'lite-ts-enum';
-
-export abstract class LoadRedisEnumHandlerBase extends LoadEnumHandlerBase {
-    public abstract flush(name: string): Promise<void>;
-}

972d5e5a38bc9c313b5709ae2413111e109279ca
连建伟 <3478289084@qq.com>
2023-03-21 11:44:30 +0800

    feat: 代码优化,提取RedisCache

--- src/i-cache.ts
+++ src/i-cache.ts
@@ -1,3 +1,5 @@
 export interface ICache {
     flush(name: string, areaNo?: number): Promise<void>;
+
+    get(name: string, areaNo: number, loadFunc: () => Promise<any>): Promise<any>;
 }
--- src/load-cache-config-handler.ts
+++ src/load-cache-config-handler.ts
@@ -1,66 +1,18 @@
 import { LoadConfigHandleOption, LoadConfigHandlerBase } from 'lite-ts-config';
 
 import { ICache } from './i-cache';
-import { RedisBase } from './redis-base';
 
-export class LoadRedisCacheConfigHandler extends LoadConfigHandlerBase implements ICache {
-    private m_Cache: {
-        [areaNo: number]: {
-            [key: string]: {
-                nextCheckOn: number;
-                updateOn: number;
-                data: any;
-            };
-        };
-    } = {};
-
+export class LoadRedisCacheConfigHandler extends LoadConfigHandlerBase {
     public constructor(
-        private m_Redis: RedisBase,
-        private m_TimeField: string,
+        private m_Cache: ICache,
     ) {
         super();
     }
 
-    public async flush(name: string, areaNo = 0) {
-        const now = Date.now();
-        if (this.m_Cache?.[areaNo]?.[name])
-            this.m_Cache[areaNo][name].nextCheckOn = now;
-
-        await this.m_Redis.hset(
-            `cache:${areaNo}`,
-            `${this.m_TimeField}:${name}`,
-            now.toString()
-        );
-    }
-
     public async handle(opt: LoadConfigHandleOption) {
-        opt.areaNo ??= 0;
-        this.m_Cache[opt.areaNo] ??= {};
-        this.m_Cache[opt.areaNo][opt.name] ??= {
-            nextCheckOn: 0,
-            updateOn: 0,
-            data: null
-        };
-
-        const now = Date.now();
-        if (this.m_Cache[opt.areaNo][opt.name].nextCheckOn < now) {
-            const value = await this.m_Redis.hget(`cache:${opt.areaNo}`, `${this.m_TimeField}:${opt.name}`);
-            const lastCacheOn = parseInt(value) || now;
-            if (this.m_Cache[opt.areaNo][opt.name].updateOn != lastCacheOn) {
-                this.m_Cache[opt.areaNo][opt.name].updateOn = lastCacheOn;
-                const optData: LoadConfigHandleOption = {
-                    name: opt.name,
-                    areaNo: opt.areaNo
-                };
-                await this.next?.handle?.(optData);
-                this.m_Cache[opt.areaNo][opt.name].data = optData.res;
-            }
-
-            this.m_Cache[opt.areaNo][opt.name].nextCheckOn = now + 5_000 + Math.floor(
-                Math.random() * 55_000
-            );
-        }
-
-        opt.res = this.m_Cache[opt.areaNo][opt.name].data;
+        opt.res = await this.m_Cache.get(opt.name, opt.areaNo, async () => {
+            await this.next?.handle?.(opt);
+            return opt.res;
+        });
     }
 }
